88a0a06bc15008f6cc765fe9f4cf1536f44a6269
Henrique Azevedo <henriqueazevedoandrade@gmail.com>
2026-03-28 17:54:11 -0300

    fix: correção da rota após login

--- finance-manager/financas/urls.py
+++ finance-manager/financas/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.lista_lancamentos, name='inicio'),
-    path('lista_lancamentos/', views.lista_lancamentos, name='lista_lancamentos'),
+    path('', views.lista_lancamentos, name='lista_lancamentos'),
     path('novo/', views.criar_lancamento, name='criar_lancamento'),
     path('editar/<int:id>/', views.editar_lancamento, name='editar_lancamento'),
     path('deletar/<int:id>/', views.deletar_lancamento, name='deletar_lancamento'),
